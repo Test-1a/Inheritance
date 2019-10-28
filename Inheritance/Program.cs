ccf43d9ee5d952f5d89ab7d1169f1fc92358ddca
Mikael <test.kurs@hotmail.net>
2019-10-28 15:15:01 -0700

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle("ABC123");
+            Vehicle bike = new Bicycle("BBB333");
+
+            Console.WriteLine(vehicle.Drive());
+            Console.WriteLine("------------------");
+            var result = bike.Drive();
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Vehicle : IVehicle
+    {
+        public Vehicle(string regNo)
+        {
+            RegNo = regNo;
+        }
+
+        public string RegNo { get; set; }
+
+        public virtual string Drive()     //virtual -> now we can do override
+        {
+           return "Vehicle starts driving";
+        }
+    }
+
+    class Bicycle : Vehicle
+    {
+        public Bicycle(string frameNumber) :base(frameNumber)
+        {
+
+        }
+
+        public override string Drive()
+        {
+            //base.Drive();
+            return base.Drive() + ", Bicycle starts pedaling";
+        }
+       
+    }
+}
